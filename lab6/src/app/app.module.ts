e0e0a03bcebb5ec591b27c2a99a8ef1e41faa3ff
21B030917 <98811495+21B030917@users.noreply.github.com>
2023-03-08 16:11:20 +0600

    i did home, albums, about us page and create search, delete photos

---
+++ lab6/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { AlbumsComponent } from './pages/albums/albums.component';
+import { AlbumDetailComponent } from './details/album-detail/album-detail.component';
+import { AlbumPhotosComponent } from './details/album-photos/album-photos.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+// import { NewComponentComponent } from './new-component/new-component.component';
+// import { DetailsComponent } from './details/details.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    AlbumsComponent,
+    AlbumDetailComponent,
+    AlbumPhotosComponent,
+    NotFoundComponent,
+    // NewComponentComponent,
+    // DetailsComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

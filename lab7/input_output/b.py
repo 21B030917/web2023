692654db67b03e98f3640665fc69a447307afd85
21B030917 <98811495+21B030917@users.noreply.github.com>
2023-03-16 02:38:01 +0600

    i did tasks in informatics about conditional operators, input and output

---
+++ lab7/input_output/b.py
@@ -0,0 +1,8 @@
+# a = int(input())
+
+# print(f'The next number for the number {a} is {a+1} .')
+# print(f'The previous number for the number {a} is {a-1}.')
+
+n = int(input())
+print('The next number for the number ' + str(n) + ' is ' + str(n + 1) + '.')
+print('The previous number for the number ' + str(n) + ' is ' + str(n - 1) + '.')
